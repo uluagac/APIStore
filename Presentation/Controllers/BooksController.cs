b6418c5b5790bd6135467403e98c4b1697f4340c
uluagac <ersinuluagac@gmail.com>
2025-03-14 01:22:35 +0300

    created servicemanager and presentation layer

--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -1,18 +1,22 @@
 ﻿using Entities.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
-using Repositories.Contracts;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace WebApi.Controllers
+namespace Presentation.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/books")]
     public class BooksController : ControllerBase
     {
-        private readonly IRepositoryManager _manager;
+        private readonly IServiceManager _manager;
 
-        public BooksController(IRepositoryManager manager)
+        public BooksController(IServiceManager manager)
         {
             _manager = manager;
         }
@@ -22,7 +26,7 @@ namespace WebApi.Controllers
         {
             try
             {
-                var books = _manager.Book.GetAllBooks(false);
+                var books = _manager.BookService.GetAllBooks(false);
                 return Ok(books);
             }
             catch (Exception ex)
@@ -36,7 +40,7 @@ namespace WebApi.Controllers
         {
             try
             {
-                var book = _manager.Book.GetOneBooksById(id, false);
+                var book = _manager.BookService.GetOneBookById(id, false);
                 if (book is null)
                 {
                     return NotFound();
@@ -56,8 +60,7 @@ namespace WebApi.Controllers
             {
                 if (book is null)
                     return BadRequest();
-                _manager.Book.CreateOneBook(book);
-                _manager.Save();
+                _manager.BookService.CreateOneBook(book);
                 return StatusCode(201, book);
             }
             catch (Exception ex)
@@ -71,15 +74,10 @@ namespace WebApi.Controllers
         {
             try
             {
-                var entity = _manager.Book.GetOneBooksById(id, true);
-                if (entity is null)
-                    return NotFound();
-                if (id != book.Id)
+                if (book is null)
                     return BadRequest();
-                entity.Title = book.Title;
-                entity.Price = book.Price;
-                _manager.Save();
-                return Ok(book);
+                _manager.BookService.UpdateOneBook(id, book, false);
+                return NoContent();
             }
             catch (Exception ex)
             {
@@ -92,15 +90,7 @@ namespace WebApi.Controllers
         {
             try
             {
-                var entity = _manager.Book.GetOneBooksById(id, false);
-                if (entity is null)
-                    return NotFound(new
-                    {
-                        StatusCode = 404,
-                        message = $"Book with id:{id} could not found."
-                    });
-                _manager.Book.DeleteOneBook(entity);
-                _manager.Save();
+                _manager.BookService.DeleteOneBook(id, false);
                 return NoContent();
             }
             catch (Exception ex)
@@ -114,11 +104,11 @@ namespace WebApi.Controllers
         {
             try
             {
-                var entity = _manager.Book.GetOneBooksById(id, true);
+                var entity = _manager.BookService.GetOneBookById(id, true);
                 if (entity is null)
                     return NotFound();
-                bookPatch.ApplyTo(entity, ModelState);
-                _manager.Book.Update(entity);
+                bookPatch.ApplyTo(entity);
+                _manager.BookService.UpdateOneBook(id, entity, true);
                 return NoContent();
             }
             catch (Exception ex)
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -2,41 +2,33 @@ using Microsoft.EntityFrameworkCore;
 using Repositories.EFCore;
 using WebApi.Extensions;
 
-namespace WebApi
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
+    .AddNewtonsoftJson();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers()
-                .AddNewtonsoftJson();
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.ConfigureSqlContext(builder.Configuration);
-            builder.Services.ConfigureRepositoryManager();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
