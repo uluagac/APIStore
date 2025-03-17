38879628321801edcdf3bafee91ec26affb6d406
uluagac <ersinuluagac@gmail.com>
2025-03-17 21:43:37 +0300

    NLog

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -12,16 +12,16 @@ namespace Services
     public class BookManager : IBookService
     {
         private readonly IRepositoryManager _manager;
+        private readonly ILoggerService _logger;
 
-        public BookManager(IRepositoryManager manager)
+        public BookManager(IRepositoryManager manager, ILoggerService logger)
         {
             _manager = manager;
+            _logger = logger;
         }
 
         public Book CreateOneBook(Book book)
         {
-            if (book is null)
-                throw new ArgumentNullException(nameof(book));
             _manager.Book.CreateOneBook(book);
             _manager.Save();
             return book;
@@ -32,7 +32,11 @@ namespace Services
             // check entity
             var entity = _manager.Book.GetOneBooksById(id, trackChanges);
             if (entity is null)
-                throw new Exception($"Book with id {id} could not found.");
+            {
+                string message = $"Book with id {id} could not found.";
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
             _manager.Book.DeleteOneBook(entity);
             _manager.Save();
         }
@@ -52,10 +56,12 @@ namespace Services
             // check entity
             var entity = _manager.Book.GetOneBooksById(id, trackChanges);
             if (entity is null)
-                throw new Exception($"Book with id {id} could not found.");
-            // check params
-            if(book is null)
-                throw new ArgumentNullException(nameof(book));
+            {
+                string message = $"Book with id {id} could not found.";
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
+
             // mapping
             entity.Title = book.Title;
             entity.Price = book.Price;
